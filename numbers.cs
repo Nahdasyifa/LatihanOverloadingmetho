4ab329f2db18d17e5e22a4a5b7a5cc0ade31fbef
Nahdasyifa <101632340+Nahdasyifa@users.noreply.github.com>
2022-04-07 22:14:05 +0700

    Add files via upload

---
+++ numbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        int totalmin;
+        int totalmax;
+
+        public int FindMinimum(int number1, int number2)
+        {
+            totalmin = number1;
+            if (totalmin > number2)
+                totalmin = number2;
+            return totalmin;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            totalmin = number1;
+            if (totalmin > number2)
+                totalmin = number2;
+            if (totalmin > (int)number3)
+                totalmin = (int)number3;
+            return totalmin;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            totalmax = number1;
+            if (totalmax > number2)
+                totalmax = number2;
+            return totalmax;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            totalmax = number1;
+            if (totalmax > number2)
+                totalmax = number2;
+            if (totalmax > (int)number3)
+                totalmax = (int)number3;
+            return totalmax;
+        }
+    }
+}
